3f6c712b03a3a074209d4172188257839b60d2d2
tam11a <ibrahimsadiktamim@gmail.com>
2025-12-29 05:54:26 -0600

    npm cache settings cause build error due to suspense boundary issues. need to explore in details about it more.

--- app/(private)/layout.tsx
+++ app/(private)/layout.tsx
@@ -7,6 +7,7 @@ import {
   SidebarTrigger,
 } from "@/components/ui/sidebar";
 import { DynamicBreadcrumb } from "@/components/dynamic-breadcrumb";
+import { Suspense } from "react";
 
 export default function PrivateLayout({
   children,
@@ -14,23 +15,25 @@ export default function PrivateLayout({
   children: React.ReactNode;
 }) {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <header className="flex shrink-0 items-center gap-2 pt-2 pb-4">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator
-              orientation="vertical"
-              className="mr-2 data-[orientation=vertical]:h-4"
-            />
-            <DynamicBreadcrumb />
-          </div>
-        </header>
-        <SidebarInsetContent className="overflow-y-auto">
-          {children}
-        </SidebarInsetContent>
-      </SidebarInset>
-    </SidebarProvider>
+    <Suspense fallback={<></>}>
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <header className="flex shrink-0 items-center gap-2 pt-2 pb-4">
+            <div className="flex items-center gap-2 px-4">
+              <SidebarTrigger className="-ml-1" />
+              <Separator
+                orientation="vertical"
+                className="mr-2 data-[orientation=vertical]:h-4"
+              />
+              <DynamicBreadcrumb />
+            </div>
+          </header>
+          <SidebarInsetContent className="overflow-y-auto">
+            {children}
+          </SidebarInsetContent>
+        </SidebarInset>
+      </SidebarProvider>
+    </Suspense>
   );
 }
